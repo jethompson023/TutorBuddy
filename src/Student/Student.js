f0948d39324f29eccb089796a62457d401180621
Jeremiah Thompson <32300456+jethompson023@users.noreply.github.com>
2023-05-10 21:28:45 +0000

    Added folders for students and tutor pages.

---
+++ src/Student/Student.js
@@ -0,0 +1,13 @@
+import '/workspaces/codespaces-react/src/Student/Student.css';
+
+function Student() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default Student;
