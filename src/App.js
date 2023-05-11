428fad21b38085bc594e3fda278e45571eef7fe9
Jeremiah Thompson <32300456+jethompson023@users.noreply.github.com>
2023-05-11 19:10:05 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ function App() {
     <div className="App">
       <body>
         
-        <main className="App-header">
+        {/* <main className="App-header">
           <h1>Welcome to Tutor Buddy</h1>
-          <h2>So you can find a buddy so homework isn't funny</h2>
-        </main>
+          <h3>So you can find a buddy so homework isn't funny</h3>
+        </main> */}
 
         <nav className="navbar">
-
+          <h2 className='logo'>tutor<span>Buddy</span></h2>
+          <u1>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Tutors</a></li>
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Search</a></li>
+          </u1>
+          <button type="button">Sign In</button>
         </nav>
       </body>
     </div>
