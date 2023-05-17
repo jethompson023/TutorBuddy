0109744546e85d7b122f3e9e285d27358ca0c1bc
Jeremiah Thompson <32300456+jethompson023@users.noreply.github.com>
2023-05-17 21:24:08 +0000

    quick update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 // import profile1 from "src/assets/profile-img/profile1.jpg";
+// import SignUp from './Forms/signUp';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
             <li><a href="#">Sessions</a></li>
             <li><a href="#">Courses</a></li>
           </u1>
-          <button onclick="src/Forms/signUp.js">Click Here</button>
+          <button type="button" onclick="src/Forms/signUp.js">Click Here</button>
           {/* <button type="button"><li><a href="src/Forms/signUp.js"></a>Sign Up</li></button> */}
           {/* <li><a href="src/Forms/signUp.js"></a>Sign Up</li> */}
           {/* <button type="button"><></>Sign Up</button> */}
@@ -30,8 +31,26 @@ function App() {
 
         </div>
 
-        <section>
+        
 
+        <section>
+          <div id="tutor-carousel">
+            {/* <h2>hello</h2> */}
+            <div className='slide-container'>
+              <div className='slide-content'>
+                  <div className='card-wrapper'>
+                      <div className='card'>
+                          <div className='image-content'>
+                              <span className='overlay'></span>
+                              <div className='card-image'>
+                                  <img src="src/Tutor/profile-img/profile1.jpg" alt="" className='card-img'></img>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
         </section>
         
         {/* <div className='slide-container'>
