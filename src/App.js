0e18cd20b4b94c6579b58f6ee931b144ed6fd43d
Lorenzo Ramirez <lenchetoramirez16@gmail.com>
2023-05-17 22:12:40 +0000

    Page Navigation Enhanced SignUp page

--- src/App.js
+++ src/App.js
@@ -1,76 +1,76 @@
-import './App.css';
-// import profile1 from "src/assets/profile-img/profile1.jpg";
-// import SignUp from './Forms/signUp';
+// import './App.css';
+// // import profile1 from "src/assets/profile-img/profile1.jpg";
+// // import SignUp from './Forms/signUp';
 
-function App() {
-  return (
-    <div className="App">
-      <body>
+// function App() {
+//   return (
+//     <div className="App">
+//       <body>
         
-        {/* <main className="App-header">
-          <h1>Welcome to Tutor Buddy</h1>
-          <h3>So you can find a buddy so homework isn't funny</h3>
-        </main> */}
+//         {/* <main className="App-header">
+//           <h1>Welcome to Tutor Buddy</h1>
+//           <h3>So you can find a buddy so homework isn't funny</h3>
+//         </main> */}
 
-        <nav className="navbar">
-          <h2 className='logo'>tutor<span>Buddy</span></h2>
-          <u1>
-            <li><a href="./App.js">Home</a></li>
-            <li><a href="src/Tutor/tutor.js">Tutors</a></li>
-            <li><a href="#">Sessions</a></li>
-            <li><a href="#">Courses</a></li>
-          </u1>
-          <button type="button" onclick="src/Forms/signUp.js">Click Here</button>
-          {/* <button type="button"><li><a href="src/Forms/signUp.js"></a>Sign Up</li></button> */}
-          {/* <li><a href="src/Forms/signUp.js"></a>Sign Up</li> */}
-          {/* <button type="button"><></>Sign Up</button> */}
-        </nav>
+//         <nav className="navbar">
+//           <h2 className='logo'>tutor<span>Buddy</span></h2>
+//           <u1>
+//             <li><a href="./App.js">Home</a></li>
+//             <li><a href="src/Tutor/tutor.js">Tutors</a></li>
+//             <li><a href="#">Sessions</a></li>
+//             <li><a href="#">Courses</a></li>
+//           </u1>
+//           <button type="button" onclick="src/Forms/signUp.js">Click Here</button>
+//           {/* <button type="button"><li><a href="src/Forms/signUp.js"></a>Sign Up</li></button> */}
+//           {/* <li><a href="src/Forms/signUp.js"></a>Sign Up</li> */}
+//           {/* <button type="button"><></>Sign Up</button> */}
+//         </nav>
 
-        <div className='calendar'>
+//         <div className='calendar'>
           
 
-        </div>
+//         </div>
 
         
 
-        <section>
-          <div id="tutor-carousel">
-            {/* <h2>hello</h2> */}
-            <div className='slide-container'>
-              <div className='slide-content'>
-                  <div className='card-wrapper'>
-                      <div className='card'>
-                          <div className='image-content'>
-                              <span className='overlay'></span>
-                              <div className='card-image'>
-                                  <img src="src/Tutor/profile-img/profile1.jpg" alt="" className='card-img'></img>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-            </div>
-          </div>
-        </section>
+//         <section>
+//           <div id="tutor-carousel">
+//             {/* <h2>hello</h2> */}
+//             <div className='slide-container'>
+//               <div className='slide-content'>
+//                   <div className='card-wrapper'>
+//                       <div className='card'>
+//                           <div className='image-content'>
+//                               <span className='overlay'></span>
+//                               <div className='card-image'>
+//                                   <img src="src/Tutor/profile-img/profile1.jpg" alt="" className='card-img'></img>
+//                               </div>
+//                           </div>
+//                       </div>
+//                   </div>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
         
-        {/* <div className='slide-container'>
-            <div className='slide-content'>
-                <div className='card-wrapper'>
-                    <div className='card'>
-                        <div className='image-content'>
-                            <span className='overlay'></span>
-                            <div className='card-image'>
-                                <img src="src/Tutor/profile-img/profile1.jpg" alt="" className='card-img'></img>
+//         {/* <div className='slide-container'>
+//             <div className='slide-content'>
+//                 <div className='card-wrapper'>
+//                     <div className='card'>
+//                         <div className='image-content'>
+//                             <span className='overlay'></span>
+//                             <div className='card-image'>
+//                                 <img src="src/Tutor/profile-img/profile1.jpg" alt="" className='card-img'></img>
 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> */}
-      </body>
-    </div>
-  );
-}
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div> */}
+//       </body>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Home from './Home/home';
+import SignUp from './SignUp/signUp';
+import NoPage from './404Page/404Page';
 import reportWebVitals from './reportWebVitals';
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<SignUp />}/>
+        {/* <Route path="home" element={<Home />} /> */}
+        <Route path="*" element={<NoPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
