b8558d46701579ea6c51d4e64d0936b6d6db809c
Lorenzo Ramirez <lenchetoramirez16@gmail.com>
2023-05-22 00:12:53 +0000

    Fixed CSS Inheritance Issues All html tags have their own classNames Added tutors folder and files

--- src/Forms/signUp.js
+++ src/Forms/signUp.js
@@ -3,36 +3,35 @@ import { Link } from "react-router-dom";
 
 function SignUp() {
   return (
-    <div class="container">
+    <div className="container">
         <head>
           <title>Sign Up</title>
         </head>
         
-        <body>
-          <div class="title">
-            <h2 class='logo'>tutor<span>Buddy</span> <br /> </h2>
-            <h6>Contact a tutor buddy for them to be a better teacher.</h6>
+        <body className="signUp-body">
+          <div className="logo-group">
+            <h2 className='logo-name'>tutor<span className='logo-buddy'>Buddy</span> <br /> </h2>
+            <h6 className="logo-subtitle">Contact a tutor buddy for them to be a better teacher.</h6>
           </div>
 
           {/* <form action="signUp.php"method="post"> */}
-          <form action="">  
-            <input type="text" name="email" placeholder="Email" class="text-box-input"></input> 
+          <form className="signUp-form" action="">  
+            <input type="text" name="email" placeholder="Email" className="text-box-input"></input> 
             <br /><br />
-            <input type="password" name="password" placeholder="Password" class="text-box-input"></input>
+            <input type="password" name="password" placeholder="Password" className="text-box-input"></input>
             <br /><br />
             <input type="checkbox" name="remember-me"></input>
             <label> Remember Me </label>
             <br /><br />
-            <Link to="/home" className="link-home"><button type="submit">Sign Up</button></Link>
-            {/* <Link to="./Home/home">Sign Up</Link> */}
-            <a href="signUp.js">Forgot Password?</a>
-            <br /><br />
+            <Link to="/Home"><button type="submit" className="general-button">Sign Up</button></Link>
+            
+            <div className="password-box"><Link to=""> <h1 className="link-password">Forgot Password?</h1></Link></div>
             <hr/>
             <br />
-            <button type="button" class="google-signin">Sign Up with Google</button>
+            <Link to="" ><button className="general-button">Sign Up with Google</button></Link>
           </form>
 
-          <footer>
+          <footer className="footer-signUp">
             Created by Audrey, Jeremiah, Lorenzo, Nathan, and Patrick <br></br> Est. 2023
           </footer>
 
---
+++ src/Tutors/tutors.js
@@ -0,0 +1,18 @@
+import './tutors.css';
+import { Link } from "react-router-dom";
+
+function Tutor() {
+  return (
+    <div className="container">
+        <head>
+          <title>Tutor</title>
+        </head>
+
+        <body>
+            <h2>Something cool</h2>
+        </body>
+    </div>
+  );
+}
+
+export default Tutor;
